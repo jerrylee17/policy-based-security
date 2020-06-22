3a24c103934cbce99a53f0b1c6d3dc07b878422f
Pratik Baniya <prabaniy@Pratiks-MacBook-Pro.local>
2020-06-21 20:45:16 -0700

    .

--- opencv/utils.py
+++ opencv/utils.py
@@ -2,19 +2,21 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3 import Retry
 
+
 def _requests_retry(url, method, **kwargs):
+    """Method for request retry"""
     retries = Retry(total=5, backoff_factor=1)
     s = requests.Session()
-    s.mount('http://', HTTPAdapter(max_retries=retries))
+    s.mount("http://", HTTPAdapter(max_retries=retries))
     response = None
-    if method in ['GET']:
-        kwargs['headers'] = {
-            'Content-Type': 'application/json'
-        }
+    if method in ["GET"]:
+        kwargs["headers"] = {"Content-Type": "application/json"}
         response = s.request(method=method, url=url, **kwargs)
+    return response
 
-if __name__ === "__main__":
+
+if __name__ == "__main__":
     for i in range(5):
-        res = _requests_retry('localhost:5000', 'GET')
+        res = _requests_retry("localhost:5000", "GET")
         if res is None:
-            print('Err')
+            print("Err")
